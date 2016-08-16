5d1cb64c52adaa786404e283afb46b340238bc49
zgambrell <root@duoadmins-MacBook-Pro-92.local>
2016-08-16 15:34:21 -0400

    message

--- test/branchdiff.sh
+++ test/branchdiff.sh
@@ -7,6 +7,10 @@ then
     echo branches are equal
     sudo git checkout dev
     sudo git pull origin dev
+    sudo git push origin dev
+    sudo git checkout staging
+    sudo git pull origin staging
+    sudo git merge dev
     sudo git push origin staging
 else
     echo branches are not equal
