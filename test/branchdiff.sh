01852bfc6d479a85d60d7fc87062c97e51c8bfdf
Zach <zgambrell@duosecurity.com>
2016-10-11 13:56:16 -0400

    v2 of erthng

---
+++ test/branchdiff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+var=$(git diff origin/staging origin/master 2>&1)
+echo $var
+if [ "$var" == "" ]
+then
+    echo branches are equal
+    sudo git checkout dev
+    sudo git pull origin dev
+    sudo git push origin staging
+else
+    echo branches are not equal
+fi
